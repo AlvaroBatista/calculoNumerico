e2b69a9b808e45b7e44011437e80cec83c5a5ccd
Alvaro Batista <alvarobatista87@outlook.com>
2021-04-06 23:11:20 -0300

    metodo da secante

--- atividade.js
+++ atividade.js
@@ -6,16 +6,20 @@ console.log('');
 // bissecao(1, 2, 0.001);
 // posicaofalsa(1, 2, 0.001);
 
-bissecao(2.4, 2.6, 0.0001);
-posicaofalsa(2.4, 2.6, 0.0001);
+// bissecao(2.4, 2.6, 0.0001);
+// posicaofalsa(2.4, 2.6, 0.0001);
 
-// bissecao(0, 2, 0.0001);
-// posicaofalsa(0, 2, 0.0001);
+bissecao(0, 2, 0.0001);
+posicaofalsa(0, 2, 0.0001);
+secante(f, 0, 2, 0.0001, 50);
+
+// secante(f, 0, 1, 0.0005, 50);
 
 function f(x) {
     // return ((x**3) + (4*(x**2)) - 10);
     return ((2.71828182846**x) - 5*x);
     // return (3*(x**3) - 4);
+    // return x**3 - 9*x + 3;
 }
 
 function bissecao(a, b, tol) {
@@ -74,27 +78,32 @@ function posicaofalsa(a, b, tol) {
     return console.log('Raiz Aproximada: ', c);
 }
 
-// function bissecao(a, b, tol) {
-//     console.log('------------------------ // ------------------------');
-//     console.log('------------ Método da Bisseção ------------');
-//     console.log('------------------------ // ------------------------');
+function secante(f, x0, x1, tol, iterMax) {
+    console.log('------------------------ // ------------------------');
+    console.log('------------ Método da Secante ------------');
+    console.log('------------------------ // ------------------------');
+    if (Math.abs(f(x0)) <= tol) {
+        return (false, x0);
+    } else if (Math.abs(f(x1)) <= tol) {
+        return (false, x1);
+    }
 
-//     if (f(a)*f(b) < 0) {
-//         var i = 0;
-//         while (Math.abs(b-a)/2 > tol) {
-//             xi = (a+b)/2;
-//             if (f(xi) == 0 ) {
-//                 console.log(' A raiz é: ', xi);
-//                 break;
-//             } else if (f(a)*f(xi) < 0) {
-//                 b = xi;
-//             } else {
-//                 a = xi;
-//             }
-//             console.log('[I]: ', i++,'|| Valor da Raiz: ', xi, 'f(x): ', f(xi));
-//         }
-//         return console.log('Raiz Aproximada: ', xi);
-//     } else {
-//         console.log('Não há raiz neste intervalo!');
-//     }
-// }
+    console.log('[x0]:', x0  ,'[f(x0)]:', f(x0));
+    console.log('[x1]:', x1  ,'[f(x1)]:', f(x1));
+
+    for (var i = 1; i <= iterMax; i++) {
+        x2 = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0));
+        console.log('[I]:', i,'[x2]:', x2, '[f(x2)]:', f(x2));
+
+        if (Math.abs(f(x2)) <= tol) {
+            return console.log('Raiz encontrada: ',x2);
+        } 
+
+        x0 = x1;
+        x1 = x2;
+    }
+
+    console.log('Número máximo de iterações atingido.');
+    console.log('Raiz encontrada:', x2);
+
+}
